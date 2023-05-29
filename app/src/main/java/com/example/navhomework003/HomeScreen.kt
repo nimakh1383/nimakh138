ee5e7299c243ea50736ea23cc74e008f93b6ac6d
p1pc1 <aligohargazi644@gmail.com>
2023-05-29 12:56:22 +0430

    first commit

---
+++ app/src/main/java/com/example/navhomework003/HomeScreen.kt
@@ -0,0 +1,43 @@
+package com.example.navhomework003
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navcont: NavHostController) {
+    var username: String by remember { mutableStateOf("") }
+    var password : String by remember { mutableStateOf("") }
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        TextField(
+            value = username,
+            onValueChange = {username=it},
+            label = { Text(text = "Username") },
+        )
+        TextField(
+            value = password,
+            onValueChange = {password=it},
+            label = { Text(text = "Password") },
+        )
+        val context1= LocalContext.current
+        Button(onClick = {
+            if (username=="tvu" && password=="111111"){
+                navcont.navigate("Second/$username")
+            }else{
+                Toast.makeText(context1,"Wrong!",Toast.LENGTH_SHORT).show()
+            }
+        }, colors = ButtonDefaults.buttonColors(Color(0xFF495E57))) {
+            Text(text = "Login")
+        }
+    }
+}
